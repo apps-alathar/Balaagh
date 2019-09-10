4b1ab48076047d4a80aeb47bd3f24363755da82c
apps-alathar <apps.alathar@gmail.com>
2019-09-09 22:28:16 -0400

    Updated and fixed about contact and home defects

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 
 /**
  * Generated class for the SettingsPage page.
@@ -15,11 +15,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public app: App ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Settings Page');
   }
 
+  logout(){
+    //API Token logout
+    const root = this.app.getRootNav();
+    root.popToRoot();
+  }
+
+  
 }
